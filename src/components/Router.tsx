3bd8fc6d144dc04029688268858eab6eaa5c2e1f
Sahar <96saharyosef@gmail.com>
2025-08-28 23:24:09 +0300

    removed Landing feature and RegisterForm, RegisterModal, FormInput components

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,14 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ChatBot from '../features/ChatBot';
 import Layout from '../Layout';
-import Landing from '@/features/Landing';
 
 const Router = () => (
 	<BrowserRouter>
 		<Routes>
 			<Route path="/" element={<Layout />}>
-				<Route index element={<Landing />} />
-				<Route path="chatbot" element={<ChatBot />} />
+				<Route index element={<ChatBot />} />
 			</Route>
 		</Routes>
 	</BrowserRouter>
